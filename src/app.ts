241f4e3e099a039b51899da13238091519359a20
walk8243 <walk8243@gmail.com>
2022-01-10 17:02:42 +0900

    middleware周りの調整

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,24 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import favicon from 'serve-favicon';
+import path from 'path';
 import { logger } from '@walk8243/logger';
 
 export const app = express();
 app.locals['port'] = parseInt(process.env['PORT'] || '3000');
 
+app
+	.use(express.json())
+	.use(express.urlencoded({ extended: false }))
+	.use(cookieParser())
+	.use(favicon(path.resolve(__dirname, '../favicon.ico')));
+
 app.use((req, _, next) => {
 	logger.debug('*'.repeat(30));
-	logger.debug(`${req.method} ${req.url}`);
+	logger.debug(`${req.method} ${req.path}`);
 	logger.debug(`[header] ${JSON.stringify(req.headers)}`);
-	if(req.body) logger.debug(`[body] ${JSON.stringify(req.body)}`);
+	logger.debug(`[query] ${JSON.stringify(req.query)}`);
+	logger.debug(`[body] ${JSON.stringify(req.body)}`);
 	logger.debug('*'.repeat(30));
 	next();
 });
