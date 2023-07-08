c4dd2b581577d00033257c3d231d5e512fa3b749
walk8243 <walk8243@gmail.com>
2023-07-08 23:39:00 +0900

    リクエスト内容から固定のステータスコードを返すエンドポイントを作成

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import favicon from 'serve-favicon';
 import serveStatic from 'serve-static';
 import path from 'path';
+import statuses from 'statuses';
 import logger from './logger';
 
 export const app = express();
@@ -22,6 +23,15 @@ app.use((req, _, next) => {
 	logger.debug(`[body] ${JSON.stringify(req.body)}`);
 	logger.debug('*'.repeat(30));
 	next();
+});
+
+app.get('/status/:code(\\d+)', (req, res) => {
+	const code = parseInt(req.params.code);
+	const message = statuses.message[code];
+	res
+		.type(req.is('json') ? 'json' : 'text')
+		.status(code)
+		.send(req.is('json') ? { code, message } : message);
 });
 
 app.use(serveStatic(path.resolve(__dirname, '../static')));
